7d71435aa83ab6fdc215f6d0f50dac60d5de4c9a
Joey Hipolito <hi@joeyhipolito.com>
2013-09-29 01:34:29 +0800

    database and sample values

---
+++ cai_one_joey/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO questions VALUES
+(null, 'what is love', 'secret', 10),
+(null, 'what is the capital city of philippines', 'manila', 10),
+(null, 'who formulated the theory of relativity', 'albert einstein', 15),
+(null, 'what is the 5th state of matter', 'bose einstein condensate', 10),
+(null, 'what is specific heat of water', '1', 20),
+(null, 'what is the chemical formula for Zirconium', 'Zr', 15)
+
