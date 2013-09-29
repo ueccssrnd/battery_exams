c00aa08fdec5bb83a54daf68f048e53e38695853
Joey Hipolito <hi@joeyhipolito.com>
2013-09-29 11:02:34 +0800

    Merge sql files. Update README.md

--- cai_one_joey/README.md
+++ cai_one_joey/README.md
@@ -1,1 +1,18 @@
-## CAI Problem Solving by Joey Hipolito
+# CAI Problem Solving by Joey Hipolito
+
+## Instructions
+
+* Copy files from the `system` folder , and modify if needed(applicable Db.php);
+* Copy the `.htaccess` file as is
+* Copy the utils.php
+* Copy the index.php
+* leave the files you copied as is
+
+--------------------------------------
+## Controllers and Models
+
+Controllers do validation on data you pass
+Models manipulate the database
+
+
+
--- cai_one_joey/db.sql
+++ cai_one_joey/db.sql
@@ -7,3 +7,13 @@ question VARCHAR(200),
 answer VARCHAR(200),
 question_value INT
 );
+
+
+INSERT INTO questions VALUES
+(null, 'what is love', 'secret', 10),
+(null, 'what is the capital city of philippines', 'manila', 10),
+(null, 'who formulated the theory of relativity', 'albert einstein', 15),
+(null, 'what is the 5th state of matter', 'bose einstein condensate', 10),
+(null, 'what is specific heat of water', '1', 20),
+(null, 'what is the chemical formula for Zirconium', 'Zr', 15)
+
