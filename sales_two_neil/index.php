b8b43831c4f22ecc4959bffbb4792e3f5722649f
neilpeter08 <neilpeter_garcia08@yahoo.com>
2013-10-02 07:16:50 +0800

    answering sales_one_question

--- sales_two_neil/index.php
+++ sales_two_neil/index.php
@@ -36,10 +36,13 @@ and open the template in the editor.
                 }
                 $date = (string)(date('jMY'));
                 $code = strtoupper($gendesc . "-" . $rand . "-" . $date);
-                $date = date('F,j,Y');
+                $date = date('Y-m-d');
+                $quantity = $_GET['stocks'] * .22;
                 echo $code;
-              //  mysqli_query($dbc, 'INSERT INTO products(code,description,stocks,date_created,quantity) 
-              //      values "' . $code . '", "' . $_GET['description'] . '","' . $_GET['stocks'] . '","' . $date . '","' . $quantity . '";)');
+                mysqli_query($dbc, "INSERT INTO products(code, description, stocks, date_created, quantity)
+                    values ('" . $code . "', '" . $_GET['description'] . "', '" . $_GET['stocks'] . "', '" . $date . "', '" . $quantity . "');");
+            } elseif($_GET['choice']=='delete'){
+                mysqli_query($dbc, 'DELETE FROM products where code = ' . $_GET['code'] . ';');
             }
         }
         
@@ -55,7 +58,20 @@ and open the template in the editor.
             <input type="text" name="description">
             <label for="stocks">Stock Level</label>
             <input type="text" name="stocks">
+            <label for="code">Product Code</label>
+            <input type="text" name="code">
             <input type="submit"/>
         </form>
+        <?php
+            $result = mysqli_query($dbc, "SELECT * from products;");
+        ?>
+        <h1>All Questions</h1>
+        <table>
+            <tr><td>Code</td><td>Description</td><td>Stocks</td><td>Date Created</td><td>Quantity</td></tr>
+        <?php
+        while($row = $result->fetch_array(MYSQLI_ASSOC))
+                echo '<tr><td>'. $row['code'] . '</td><td>' . $row['description'] . '</td><td>' . $row['stocks'] . '</td><td>' . $row['date_created'] . '</td><td>' .$row['quantity'] . '</td></tr>'
+        ?>
+        </table>
     </body>
 </html>
