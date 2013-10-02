718b813c6c1b14b4dd9df0f846c0916c4c1df78d
neilpeter08 <neilpeter_garcia08@yahoo.com>
2013-10-02 21:46:47 +0800

    cleaned module function

--- sales_two_neil/module.php
+++ sales_two_neil/module.php
@@ -8,16 +8,13 @@
         <?php
         include 'utilities.php';
         if (isset($_GET['transaction'])) {
+            $result = mysqli_query($dbc, "SELECT quantity FROM products WHERE code = '" . $_GET['code'] . "';");
+            $res = mysqli_fetch_array($result);
+            $quant = $res['quantity'];
             if ($_GET['transaction'] == 'order') {
-                $result = mysqli_query($dbc, "SELECT quantity FROM products WHERE code = '" . $_GET['code'] . "';");
-                $res = mysqli_fetch_array($result);
-                $quant = $res['quantity'];
                 $value = $quant + $_GET['quantity'];
                 mysqli_query($dbc, "UPDATE products SET quantity = '" . $value . "' WHERE code = '" . $_GET['code'] . "';");
             } elseif ($_GET['transaction'] == "sale") {
-                $result = mysqli_query($dbc, "SELECT quantity FROM products WHERE code = '" . $_GET['code'] . "';");
-                $res = mysqli_fetch_array($result);
-                $quant = $res['quantity'];
                 $value = $_GET['quantity'] - $quant;
                 mysqli_query($dbc, "UPDATE products SET quantity = '" . $value . "' WHERE code = '" . $_GET['code'] . "';");
             }
